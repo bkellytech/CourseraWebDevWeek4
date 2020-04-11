6a29bdec9368ec6e53cfbc79cba3c90d69a95d3f
Brendan Kelly <bkellytech@yahoo.com>
2020-04-11 14:17:57 -0400

    Update SpeakHello.js
    upload homework

---
+++ docs/SpeakHello.js
@@ -0,0 +1,16 @@
+// STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
+
+
+(function (window) { 
+    // STEP 3: Create an object, called 'helloSpeaker'
+    var helloSpeaker = {};
+    var speakWord = "Hello";
+
+    // STEP 4: 
+    helloSpeaker.speak = function (name) {
+      console.log(speakWord + " " + name);
+    }
+
+    // STEP 5:
+     window.helloSpeaker = helloSpeaker;
+})(window);
